4528dee8f39abdf4d1bad104a0753eac887fafff
bhawna94110 <bhawnarajput94110@gmail.com>
2022-12-22 11:35:54 +0530

    feat: add properties, enquiries models, serializers

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -45,7 +45,7 @@ THIRD_PARTY_APPS = [
     "rest_framework_simplejwt",
 ]
 
-LOCAL_APPS = ["apps.common", "apps.profiles", "apps.users", "apps.ratings"]
+LOCAL_APPS = ["apps.common", "apps.profiles", "apps.users", "apps.ratings", "apps.properties", "apps.enquiries"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
